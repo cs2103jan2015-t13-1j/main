6c01f86b65c90f0e85a93efcfcaec81be2bce1ea
Christina Lee <christina.lwt@gmail.com>
2015-03-04 16:53:30 +0800

    Added code to read/write storage in Logic class

--- src/organizer/logic/Logic.java
+++ src/organizer/logic/Logic.java
@@ -1,16 +1,19 @@
 package organizer.logic;
 
+import java.util.ArrayList;
+
 import organizer.storage.Storage;
 
 public class Logic {
 	Storage tempStorage = new Storage();
+	ArrayList<Task> tempList = new ArrayList<Task>();
 	
 	public void loadStorage() {
-		
+		tempList = tempStorage.readFile();
 	}
 	
 	public void writeStorage() {
-		
+		tempStorage.writeFile(tempList);
 	}
 	
 	public void addTask() {
