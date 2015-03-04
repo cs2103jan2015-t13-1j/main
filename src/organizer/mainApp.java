01237e449ef46e956f1fafa9425610d7210f29c1
Pearl Sortman <pearlsortman@gmail.com>
2015-03-04 18:13:50 +0800

    deleted unnecessary methods in main

--- src/organizer/mainApp.java
+++ src/organizer/mainApp.java
@@ -7,64 +7,33 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class mainApp {
-	
+
 	public static final String MESSAGE_EMPTY = "You have no tasks.";
 	static Logic logic = new Logic();
 
 	public static void main(String[] args) {
-		displayTasks();
-		while(true) {
+		ArrayList<Task> tasksArray = logic.loadStorage();
+		displayTasks(tasksArray);
+		while (true) {
+			Scanner sc = new Scanner(System.in);
 			System.out.println("Please enter a command: ");
-			ArrayList<String> parsedInput = parseInput();
-			String command = parsedInput.get(0);
-			String inputArgs = getInputArgsString(parsedInput);
-			displayTasks(logic.executeComand());
+			String userInput = sc.nextLine().toString();
+			displayTasks(logic.executeCommand(userInput));
 		}
 	}
-	
-	/*
-	 * parses user input returning as an array command will always be at 0 index
-	 */
-	private static String getInputArgsString(ArrayList<String> inputArray) {
-		if (inputArray.get(1) != null) {
-			String inputArgsString = inputArray.get(1);
-			for (int i = 2; i < inputArray.size(); i++) {
-				inputArgsString = inputArgsString + " " + inputArray.get(i);
-			}
-			return inputArgsString;
-		} else {
-			return null;
-		}
-	}
-	
-	public static void displayTasks() {
-		ArrayList<Task> tasks = logic.loadStorage();
+
+	public static void displayTasks(ArrayList<Task> tasks) {
 		if (tasks.get(0) == null) {
 			printMessage(MESSAGE_EMPTY, null, null);
 		} else {
 			for (int i = 0; i < tasks.size(); i++) {
-				System.out.print((i+1) + ". " + tasks.get(i).getTaskName());
+				System.out.print((i + 1) + ". " + tasks.get(i).getTaskName());
 				System.out.print(" " + tasks.get(i).getTaskStatus());
 				System.out.println(" " + tasks.get(i).getDueDate());
 			}
 		}
 	}
-	
-	public static ArrayList<String> parseInput() {
-		Scanner lines = new Scanner(System.in);
-		Scanner tokens = new Scanner(lines.nextLine());
-		List<String> parsedInputArray = new ArrayList<String>();
-		
-		while (tokens.hasNext()) {
-			parsedInputArray.add(tokens.next());
-		}
-		return (ArrayList<String>) parsedInputArray;
-	}
-	
-	/*
-	 * printMessage will customize output strings
-	 * defined in final class variables
-	 */
+
 	private static void printMessage(String message, String unique1,
 			String unique2) {
 		if (unique1 != null && unique2 != null) {
