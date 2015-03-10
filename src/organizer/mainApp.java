70277a9b0fc81bebfe27b3b49f1ecf9e0c3793a6
Pearl Sortman <pearlsortman@gmail.com>
2015-03-10 15:41:20 +0800

    removed random lines in main

--- src/organizer/mainApp.java
+++ src/organizer/mainApp.java
@@ -15,10 +15,6 @@ public class mainApp {
 		Logic logic = new Logic();
 		ArrayList<Task> tasksArray = logic.loadStorage();
 		displayTasks(tasksArray);
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 		
 		try (Scanner sc = new Scanner(System.in)) {
 			while (true) {
