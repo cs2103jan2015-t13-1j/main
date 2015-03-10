c21cf88faf114441c3949119fc8cd6bd9e6d1656
Pearl Sortman <pearlsortman@gmail.com>
2015-03-10 18:00:01 +0800

    added testing framework and started setting test params

--- src/organizer/test/TestAll.java
+++ src/organizer/test/TestAll.java
@@ -2,14 +2,49 @@ package organizer.test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.ArrayList;
 
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+
+import organizer.logic.Task;
 
 public class TestAll {
-
+	
+	// creates expected test data to compare against actual
+	// produces an ArrayList
+	@SuppressWarnings("null")
+	@Parameters
+	public static ArrayList<Task> taskListContainingSearchWordAssignment() {
+		ArrayList<Task> filteredTaskListOnAssignment = null;
+		Task task1 = new Task();
+		filteredTaskListOnAssignment.add(task1);
+		return filteredTaskListOnAssignment;
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddSucessfullyAddsToEndOfList() {
+		
+	}
+	
+	@Test
+	public void testDeleteRemovesCorrectLine() {
+		
+	}
+	
+	@Test
+	public void testViewMethodReturnsCompleteArrayList() {
+		
+	}
+	
+	@Test
+	public void testSearchMethodReturnsFilteredArrayList() {
+		
+	}
+	
+	@Test
+	public void testCompleteMethodMarksTaskAsComplete() {
+		
 	}
 
 }
