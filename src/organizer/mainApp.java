75fe95fdebc1138b74c41c19949d35548876d1fe
Ding Xiang Fei <dingxiangfei2009@gmail.com>
2015-03-10 13:42:20 +0800

    Refactoring mainApp and set up test cases

--- src/organizer/mainApp.java
+++ src/organizer/mainApp.java
@@ -7,42 +7,52 @@ import java.util.*;
 
 public class mainApp {
 
+	public static final String COMMAND_PROMPT = "Please enter a command: ";
 	public static final String MESSAGE_EMPTY = "You have no tasks.";
+	public static final String TASK_TEMPLATE = "%d. %s %s %s";
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
 		Logic logic = new Logic();
 		ArrayList<Task> tasksArray = logic.loadStorage();
 		displayTasks(tasksArray);
-		while (true) {
-			Scanner sc = new Scanner(System.in);
-			System.out.print("Please enter a command: ");
-			String userInput = sc.nextLine().toString();
-			displayTasks(logic.executeCommand(userInput));
+		
+		try (Scanner sc = new Scanner(System.in)) {
+			while (true) {
+				printMessage(COMMAND_PROMPT);
+				String userInput = sc.nextLine().toString();
+				displayTasks(logic.executeCommand(userInput));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
 	private static void displayTasks(ArrayList<Task> tasks) {
 		if (tasks.size() == 0) {
-			printMessage(MESSAGE_EMPTY, null, null);
+			printMessage(MESSAGE_EMPTY);
 		} else {
 			for (int i = 0; i < tasks.size(); i++) {
-				System.out.print((i + 1) + ". " + tasks.get(i).getTaskName());
-				System.out.print(" " + tasks.get(i).getTaskStatus());
-				System.out.println(" " + tasks.get(i).getDueDate());
+				final Task task = tasks.get(i);
+				printMessage(TASK_TEMPLATE,
+						i + 1,
+						task.getTaskName(),
+						task.getTaskStatus(),
+						task.getDueDate().toString());
+				printMessage();
 			}
 		}
 	}
 
-	private static void printMessage(String message, String unique1,
-			String unique2) {
-		if (unique1 != null && unique2 != null) {
-			System.out.println(String.format(message, unique1, unique2));
-		} else if (unique1 != null && unique2 == null) {
-			System.out.println(String.format(message, unique1));
-		} else {
-			System.out.println(message);
-		}
+	private static void printMessage(String message, Object... args) {
+		System.out.printf(message, args);
+	}
+	
+	private static void printMessage(String message) {
+		System.out.println(message);
+	}
+	
+	private static void printMessage() {
+		System.out.println();
 	}
 
 }
