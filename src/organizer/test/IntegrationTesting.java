d98fa69d41f32202d67b71af0ba35d144d8365af
Pearl Sortman <pearlsortman@gmail.com>
2015-04-02 12:54:47 +0800

    beginning integration testing, completed unit testing for undo command

---
+++ src/organizer/test/IntegrationTesting.java
@@ -0,0 +1,18 @@
+package organizer.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import organizer.mainApp;
+
+public class IntegrationTesting {
+
+	@Test
+	// TO TEST: 
+	public void testUserCommandExecutedProperly() {
+		mainApp main = new mainApp();
+		
+		assertEquals();
+	}
+
+}
