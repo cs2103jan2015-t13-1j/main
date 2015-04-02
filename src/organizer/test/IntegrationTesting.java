1449cd88b8fa5f39afa1588e03581795e637d14d
Ding Xiang Fei <dingxiangfei2009@gmail.com>
2015-04-02 13:51:08 +0800

    Integration test skeleton

--- src/organizer/test/IntegrationTesting.java
+++ src/organizer/test/IntegrationTesting.java
@@ -2,17 +2,44 @@ package organizer.test;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Scanner;
+
 import org.junit.Test;
+
 import organizer.mainApp;
+import organizer.logic.Logic;
+import organizer.logic.ResultSet;
+import organizer.logic.Task;
+import organizer.parser.CommandParser;
 
 public class IntegrationTesting {
 
+	
+	private static String readFile(InputStream in) {
+		final Scanner sc = new Scanner(in).useDelimiter("\\A");
+		final String result = sc.next();
+		sc.close();
+		return result;
+	}
+	
 	@Test
 	// TO TEST: 
 	public void testUserCommandExecutedProperly() {
 		mainApp main = new mainApp();
-		
-		assertEquals();
+		final Logic logic = new Logic();
+		CommandParser commandParser = new CommandParser();
+		try (Scanner sc = new Scanner(getClass().getResource("commands.txt").openStream())) {
+			while (sc.hasNext()) {
+				final ResultSet resultList = commandParser.executeCommand(sc.nextLine());
+				// compare resultList with another list
+			}
+			// compare storage
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
