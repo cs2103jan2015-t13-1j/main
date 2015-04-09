95c472e53287294f4bfe40b94a129b38e386e241
Ding Xiang Fei <dingxiangfei2009@gmail.com>
2015-04-09 20:58:51 +0800

    Add integration test suite and fix edit end time problem

--- src/organizer/test/IntegrationTesting.java
+++ src/organizer/test/IntegrationTesting.java
@@ -1,10 +1,16 @@
 package organizer.test;
 
+import static org.junit.Assert.*;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,12 +20,15 @@ import organizer.parser.CommandParser;
 
 public class IntegrationTesting {
 	CommandParser commandParser;
+	private static final Pattern MATCH_TO_END = Pattern.compile("\\Z");
 
 	private static String readStream(InputStream in) {
 		try(Scanner sc = new Scanner(in)) {
-			final String result = sc.useDelimiter("\\A").next();
+			final String result = sc.useDelimiter(MATCH_TO_END).next();
 			sc.close();
 			return result;
+		}catch(NoSuchElementException e){
+			return "";
 		}
 	}
 	
@@ -31,14 +40,18 @@ public class IntegrationTesting {
 	@Test
 	public void testUserCommandExecutedProperly() throws IOException {
 		commandParser.executeCommand("clear");
-		try (Scanner sc = new Scanner(getClass().getResource("resources/commands.txt").openStream())) {
+		try (Scanner sc = new Scanner(getClass().getResourceAsStream("resources/commands.txt"))) {
 			while (sc.hasNext()) {
 				commandParser.executeCommand(sc.nextLine());
 				// compare resultList with another list
 			}
 			// compare storage
-			final String expected = readStream(new FileInputStream("expected.txt"));
-			final String result = commandParser;
+			final Pattern expected = Pattern.compile(readStream(getClass().getResourceAsStream("resources/expected.txt")));
+			final PipedInputStream in = new PipedInputStream();
+			final PipedOutputStream out = new PipedOutputStream(in);
+			commandParser.writeStorageToStream(out);
+			final String result = readStream(in);
+			assertTrue(expected.matcher(result).find());
 		}
 	}
 
