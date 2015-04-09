bb1646de2d545df5abcc75a9b61dc1028078de01
Ding Xiang Fei <dingxiangfei2009@gmail.com>
2015-04-09 23:20:41 +0800

    Integration test now supports comparing task object through storage output

--- src/organizer/test/IntegrationTesting.java
+++ src/organizer/test/IntegrationTesting.java
@@ -8,7 +8,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -17,10 +21,13 @@ import org.junit.Test;
 
 import organizer.logic.ResultSet;
 import organizer.parser.CommandParser;
+import organizer.storage.Storage;
 
 public class IntegrationTesting {
-	CommandParser commandParser;
+	CommandParser commandParser = new CommandParser();
+	final Storage storage = new Storage();
 	private static final Pattern MATCH_TO_END = Pattern.compile("\\Z");
+	private static final Pattern MATCH_TO_EOL = Pattern.compile("<<<EOL");
 
 	private static String readStream(InputStream in) {
 		try(Scanner sc = new Scanner(in)) {
@@ -34,16 +41,34 @@ public class IntegrationTesting {
 	
 	@Before
 	public void initialise() {
-		commandParser = new CommandParser();
 	}
 	
 	@Test
 	public void testUserCommandExecutedProperly() throws IOException {
 		commandParser.executeCommand("clear");
+		final Queue<Integer> queueLine = new LinkedList<>();
+		final Queue<Pattern> queueExpected = new LinkedList<>();
+		try (Scanner sc = new Scanner(getClass().getResourceAsStream("resources/compare_view.txt"))) {
+			while(sc.hasNextInt()) {
+				queueLine.add(sc.nextInt());
+				queueExpected.add(Pattern.compile(sc.useDelimiter(MATCH_TO_EOL).next().trim()));
+				sc.reset().skip(MATCH_TO_EOL);
+			}
+		}
 		try (Scanner sc = new Scanner(getClass().getResourceAsStream("resources/commands.txt"))) {
-			while (sc.hasNext()) {
-				commandParser.executeCommand(sc.nextLine());
+			for (int line = 0; sc.hasNext(); ++line) {
+				final ResultSet rs = commandParser.executeCommand(sc.nextLine());
 				// compare resultList with another list
+				if (!queueLine.isEmpty() && line == queueLine.peek()) {
+					queueLine.poll();
+					final Pattern expected = queueExpected.poll();
+					final PipedInputStream in = new PipedInputStream();
+					final PipedOutputStream out = new PipedOutputStream(in);
+					storage.writeFileToStream(rs.getReturnList(), out);
+					final String result = readStream(in);
+					assertTrue(expected.matcher(result).find());
+					in.close();
+				}
 			}
 			// compare storage
 			final Pattern expected = Pattern.compile(readStream(getClass().getResourceAsStream("resources/expected.txt")));
